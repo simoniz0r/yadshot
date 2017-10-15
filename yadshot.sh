564f45d759d4a8edc97698ae069af9e423d23dd7
simoniz0r <bnu.fantasma@gmail.com>
2017-10-15 17:52:11 -0500

    Add upload list to main window

--- yadshot.sh
+++ yadshot.sh
@@ -33,7 +33,7 @@ YADSHOT="$RUNNING_DIR/yadshot-capture.sh"
 TRAY="$RUNNING_DIR/yadshot-tray.sh"
 
 startfunc () {
-    yad --center --title="yadshot" --text="Welcome to yadshot" --text-align="center" --height=100 --info --buttons-layout="edge" --button="Upload file/image"\!gtk-go-up:1 --button="Upload paste"\!gtk-copy:2 --button="New Screenshot"\!gtk-new:0 --button=gtk-cancel:3
+    yad --center --title="yadshot" --text="Welcome to yadshot" --text-align="center" --height=100 --info --buttons-layout="edge" --button="Upload file/image"\!gtk-go-up:1 --button="Upload paste"\!gtk-copy:2 --button="New Screenshot"\!gtk-new:0 --button="View upload list"\!gtk-edit:4 --button=gtk-cancel:3
     case $? in
         1)
             FILE="$(yad --file --center --title=yadshot)"
@@ -51,6 +51,23 @@ startfunc () {
             ;;
         0)
             $YADSHOT
+            ;;
+        4)
+            LIST_ITEM="$(yad --center --list --title="yadshot" --separator="" --column="Uploads" --button=gtk-close:2 --button="Delete list"\!gtk-delete:1 --button=gtk-copy:0 --rest="$HOME/.teknik")"
+            case $? in
+                2)
+                    sleep 0
+                    ;;
+                1)
+                    yad --center --info --title="yadshot" --button=gtk-ok --text="~/.teknik has been removed!"
+                    rm -f ~/.teknik
+                    ;;
+                0)
+                    echo "$LIST_ITEM" | xclip -selection primary
+                    echo "$LIST_ITEM" | xclip -selection clipboard
+                    yad --center --info --title="yadshot" --button=gtk-ok --text="$LIST_ITEM has been copied to clipboard."
+                    ;;
+            esac
             ;;
         3)
             exit 0
