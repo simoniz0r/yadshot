c8ea07dc4e15fc81f2e37a6071712e925a039496
simoniz0r <bnu.fantasma@gmail.com>
2018-03-27 01:21:11 -0500

    Combine scripts into one and change main screen layout

--- yadshot.sh
+++ yadshot.sh
@@ -5,8 +5,6 @@
 # License: GPL v2 Only
 # Dependencies: coreutils, slop, imagemagick, yad, xclip, curl
 
-RUNNING_DIR="$(dirname $(readlink -f $0))"
-
 if ! type curl >/dev/null 2>&1; then
     MISSING_DEPS="TRUE"
     echo "$(tput setaf 1)curl is not installed!$(tput sgr0)"
@@ -28,85 +26,271 @@ if [ "$MISSING_DEPS" = "TRUE" ]; then
     exit 1
 fi
 
-TEKNIK="$RUNNING_DIR/teknik.sh"
-YADSHOT="$RUNNING_DIR/yadshot-capture.sh"
-TRAY="$RUNNING_DIR/yadshot-tray.sh"
+export RUNNING_DIR="$(dirname $(readlink -f $0))"
+SS_NAME="yadshot$(date +'%m-%d-%y-%H%M%S').png"
+SELECTION="TRUE"
+DECORATIONS="TRUE"
+SS_DELAY=0
 
-. ~/.config/yadshot/yadshot.conf
+if [ ! -d ~/.config/yadshot ]; then
+    mkdir ~/.config/yadshot
+    echo "SELECTION="\"$SELECTION\""" > ~/.config/yadshot/yadshot.conf
+    echo "DECORATIONS="\"$DECORATIONS\""" >> ~/.config/yadshot/yadshot.conf
+    echo "SS_DELAY="\"$SS_DELAY\""" >> ~/.config/yadshot/yadshot.conf
+fi
 
-savesettingsfunc () {
+[ -f "$HOME/.config/yadshot/yadshot.conf" ] && . ~/.config/yadshot/yadshot.conf
+
+# add handler to manage process shutdown
+function on_exit() {
+    echo "quit" >&3
+    rm -f $PIPE
+}
+export -f on_exit
+
+# add handler for tray icon left click
+function on_click() {
+    "$RUNNING_DIR"/yadshot.sh
+}
+export -f on_click
+
+function teknik_file() {
+    "$RUNNING_DIR"/yadshot.sh -f
+}
+export -f teknik_file
+
+function teknik_paste() {
+    "$RUNNING_DIR"/yadshot.sh -p
+}
+export -f teknik_paste
+
+function yadshot_capture() {
+    "$RUNNING_DIR"/yadshot.sh -c
+}
+export -f yadshot_capture
+
+function upload_list() {
+    LIST_ITEM="$(yad --center --list --height 600 --width 800 --title="yadshot" --separator="" --column="Uploads" --button=gtk-close:2 --button="Delete list"\!gtk-delete:1 --button=gtk-copy:0 --rest="$HOME/.teknik")"
+    case $? in
+        2)
+            sleep 0
+            ;;
+        1)
+            yad --center --info --title="yadshot" --button=gtk-ok --text="~/.teknik has been removed!"
+            rm -f ~/.teknik
+            ;;
+        0)
+            echo -n "$LIST_ITEM" | xclip -selection primary
+            echo -n "$LIST_ITEM" | xclip -selection clipboard
+            yad --center --info --title="yadshot" --button=gtk-ok --text="$LIST_ITEM has been copied to clipboard."
+            ;;
+    esac
+}
+export -f upload_list
+
+# create the notification icon
+function yadshottray() {
+    trap on_exit EXIT
+    # create a FIFO file, used to manage the I/O redirection from shell
+    PIPE=$(mktemp -u --tmpdir ${0##*/}.XXXXXXXX)
+    mkfifo $PIPE
+    # attach a file descriptor to the file
+    exec 3<> $PIPE
+    yad --notification                  \
+        --listen                        \
+        --image="gtk-dnd"              \
+        --text="yadshot"   \
+        --command="bash -c on_click"    \
+        --item-separator=","            \
+        --menu="New Screenshot,bash -c yadshot_capture,gtk-new|Upload File,bash -c teknik_file,gtk-go-up|Upload Paste,bash -c teknik_paste,gtk-copy|View Upload List,bash -c upload_list,gtk-edit" <&3
+}
+
+function savesettings() {
     echo "SELECTION="\"$SELECTION\""" > ~/.config/yadshot/yadshot.conf
     echo "DECORATIONS="\"$DECORATIONS\""" >> ~/.config/yadshot/yadshot.conf
     echo "SS_DELAY="\"$SS_DELAY\""" >> ~/.config/yadshot/yadshot.conf
 }
 
-startfunc () {
-    OUTPUT="$(yad --center --title="yadshot" --text="Screenshot Settings:\n\n" --text-align="center" --height=200 --form --always-print-result --no-escape --separator="," --borders="10" --columns="4" --field="Capture selection":CHK "$SELECTION" --field="Capture decorations":CHK "$DECORATIONS" --field="Delay before capture":NUM "$SS_DELAY!0..120" --buttons-layout="edge" --button="New Screenshot"\!gtk-new:0 --button="Upload file/image"\!gtk-go-up:1 --button="Upload paste"\!gtk-copy:2 --button="View upload list"\!gtk-edit:4 --button=gtk-cancel:3)"
-    case $? in
+function yadshotupload() {
+    FAILED="0"
+    "$RUNNING_DIR"/teknik.sh "$1" || FAILED="1"
+}
+
+function yadshotcapture() {
+    . ~/.config/yadshot/yadshot.conf
+    if [ "$SELECTION" = "FALSE" ]; then
+        if [ $SS_DELAY -eq 0 ]; then
+            SS_DELAY=0.5
+        fi
+        sleep "$SS_DELAY"
+        if [ -f "$RUNNING_DIR/ImageMagick" ]; then
+            $RUNNING_DIR/ImageMagick import -window root /tmp/"$SS_NAME"
+        else
+            import -window root /tmp/"$SS_NAME"
+        fi
+    elif [ "$SELECTION" = "TRUE" ] && [ "$DECORATIONS" = "TRUE" ]; then
+        read -r G < <(slop --nokeyboard -lc 0,119,255,0.34 -f "%g")
+        sleep "$SS_DELAY"
+        if [ -f "$RUNNING_DIR/ImageMagick" ]; then
+            "$RUNNING_DIR"/ImageMagick import -window root -crop $G /tmp/"$SS_NAME"
+        else
+            import -window root -crop $G /tmp/"$SS_NAME"
+        fi
+    elif [ "$SELECTION" = "TRUE" ] && [ "$DECORATIONS" = "FALSE" ]; then
+        read -r G < <(slop --nokeyboard -nlc 0,119,255,0.34 -f "%g")
+        sleep "$SS_DELAY"
+        if [ -f "$RUNNING_DIR/ImageMagick" ]; then
+            "$RUNNING_DIR"/ImageMagick import -window root -crop $G /tmp/"$SS_NAME"
+        else
+            import -window root -crop $G /tmp/"$SS_NAME"
+        fi
+    fi
+}
+
+function displayss() {
+    . ~/.config/yadshot/yadshot.conf
+    WSCREEN_RES=$(xrandr | grep 'current' | cut -f2 -d"," | sed 's:current ::g' | cut -f2 -d" " | awk '{print $1 * .75}' | cut -f1 -d'.')
+    HSCREEN_RES=$(xrandr | grep 'current' | cut -f2 -d"," | sed 's:current ::g' | cut -f4 -d" " | awk '{print $1 * .75}' | cut -f1 -d'.')
+    WSIZE=$(file /tmp/$SS_NAME | cut -f2 -d"," | cut -f2 -d" " | cut -f1 -d'.')
+    HSIZE=$(file /tmp/$SS_NAME | cut -f2 -d"," | cut -f4 -d" " | cut -f1 -d'.')
+    if [ $WSCREEN_RES -le $WSIZE ] || [ $HSCREEN_RES -le $HSIZE ]; then
+        mv /tmp/"$SS_NAME" /tmp/"$SS_NAME"_ORIGINAL
+        if [ -f "$RUNNING_DIR/ImageMagick" ]; then
+            "$RUNNING_DIR"/ImageMagick convert -resize 50% /tmp/"$SS_NAME"_ORIGINAL /tmp/"$SS_NAME"
+        else
+            convert -resize 50% /tmp/"$SS_NAME"_ORIGINAL /tmp/"$SS_NAME"
+        fi
+    fi
+    OUTPUT="$(yad --center --form --always-print-result --no-escape --image="/tmp/$SS_NAME" --image-on-top --buttons-layout="edge" --title="yadshot" --separator="," --borders="10" --columns="4" --field="Capture selection":CHK "$SELECTION" --field="Capture decorations":CHK "$DECORATIONS" --field="Delay before capture":NUM "$SS_DELAY!0..120" --button="Close"\!gtk-close:1 --button="Copy to clipboard"\!gtk-paste:2 --button="Upload to teknik"\!gtk-go-up:3 --button=gtk-save:4 --button="New Screenshot"\!gtk-new:0)"
+    BUTTON_PRESSED="$?"
+    if [ -f /tmp/"$SS_NAME"_ORIGINAL ]; then
+        rm -f /tmp/"$SS_NAME"
+        mv /tmp/"$SS_NAME"_ORIGINAL /tmp/"$SS_NAME"
+    fi
+    buttonpressed
+}
+
+function buttonpressed() {
+    case $BUTTON_PRESSED in
         1)
             SELECTION="$(echo $OUTPUT | cut -f1 -d",")"
             DECORATIONS="$(echo $OUTPUT | cut -f2 -d",")"
             SS_DELAY="$(echo $OUTPUT | cut -f3 -d",")"
-            savesettingsfunc
-            FILE="$(yad --file $PWD --center --title=yadshot --height 600 --width 800)"
-            case $? in
-                0)
-                    $TEKNIK "$FILE"
-                    ;;
-                *)
-                    exit 0
-                    ;;
-            esac
+            savesettings
+            rm -f /tmp/"$SS_NAME"
+            exit 0
             ;;
         2)
             SELECTION="$(echo $OUTPUT | cut -f1 -d",")"
             DECORATIONS="$(echo $OUTPUT | cut -f2 -d",")"
             SS_DELAY="$(echo $OUTPUT | cut -f3 -d",")"
-            savesettingsfunc
-            PASTE_INPUT="$(yad --form --title="yadshot" --center --height 600 --width 800 --field="":TXT "$(xclip -o -selection -clipboard)" --button=gtk-cancel:1 --button="Upload paste"\!gtk-copy:0)"
-            case $? in
-                0)
-                    echo "$PASTE_INPUT" | xclip -i -selection -clipboard
-                    $TEKNIK -p
-                    ;;
-                *)
-                    exit 0
-                    ;;
-            esac
-            ;;
-        0)
-            SELECTION="$(echo $OUTPUT | cut -f1 -d",")"
-            DECORATIONS="$(echo $OUTPUT | cut -f2 -d",")"
-            SS_DELAY="$(echo $OUTPUT | cut -f3 -d",")"
-            savesettingsfunc
-            $YADSHOT
-            ;;
-        4)
-            SELECTION="$(echo $OUTPUT | cut -f1 -d",")"
-            DECORATIONS="$(echo $OUTPUT | cut -f2 -d",")"
-            SS_DELAY="$(echo $OUTPUT | cut -f3 -d",")"
-            savesettingsfunc
-            LIST_ITEM="$(yad --center --list --height 600 --width 800 --title="yadshot" --separator="" --column="Uploads" --button=gtk-close:2 --button="Delete list"\!gtk-delete:1 --button=gtk-copy:0 --rest="$HOME/.teknik")"
-            case $? in
-                2)
-                    sleep 0
-                    ;;
-                1)
-                    yad --center --info --title="yadshot" --button=gtk-ok --text="~/.teknik has been removed!"
-                    rm -f ~/.teknik
-                    ;;
-                0)
-                    echo -n "$LIST_ITEM" | xclip -i -selection primary
-                    echo -n "$LIST_ITEM" | xclip -i -selection clipboard
-                    yad --center --info --title="yadshot" --button=gtk-ok --text="$LIST_ITEM has been copied to clipboard."
-                    ;;
-            esac
+            savesettings
+            xclip -selection clipboard -t image/png -i < /tmp/"$SS_NAME"
+            displayss
             ;;
         3)
             SELECTION="$(echo $OUTPUT | cut -f1 -d",")"
             DECORATIONS="$(echo $OUTPUT | cut -f2 -d",")"
             SS_DELAY="$(echo $OUTPUT | cut -f3 -d",")"
-            savesettingsfunc
+            savesettings
+            cp /tmp/"$SS_NAME" $HOME/Pictures/"$SS_NAME"
+            yadshotupload "$HOME/Pictures/"$SS_NAME""
+            case $FAILED in
+                0)
+                    rm -f "$HOME/Pictures/"$SS_NAME""
+                    displayss
+                    ;;
+                1)
+                    yad --center --error --title="yadshot" --text="$SS_NAME upload failed; screenshot stored in $HOME/Pictures/"$SS_NAME""
+                    displayss
+                    ;;
+            esac
+            ;;
+        4)
+            SELECTION="$(echo $OUTPUT | cut -f1 -d",")"
+            DECORATIONS="$(echo $OUTPUT | cut -f2 -d",")"
+            SS_DELAY="$(echo $OUTPUT | cut -f3 -d",")"
+            savesettings
+            SAVE_DIR=$(yad --center --file --save --confirm-overwrite --title="yadshot" --width=800 --height=600 --text="Save $SS_NAME as...")
+            cp /tmp/"$SS_NAME" "$SAVE_DIR"
+            displayss
+            ;;
+        0)
+            rm -f /tmp/"$SS_NAME"
+            SS_NAME="yadshot$(date +'%m-%d-%y-%l%M%p').png"
+            SELECTION="$(echo $OUTPUT | cut -f1 -d",")"
+            DECORATIONS="$(echo $OUTPUT | cut -f2 -d",")"
+            SS_DELAY="$(echo $OUTPUT | cut -f3 -d",")"
+            savesettings
+            "$RUNNING_DIR"/yadshot.sh
+            ;;
+    esac
+}
+
+function startfunc() {
+    OUTPUT="$(yad --center --title="yadshot" --height=200 --form --always-print-result --no-escape --text-align="center" \
+    --separator="," --borders="10" --columns="2" --button="Ok"\!gtk-ok --button="Cancel"\!gtk-cancel:1 \
+    --field="":LBL "" --field="":CB "New Screenshot!Upload File!Upload Paste!View Upload List" --field="":LBL "" --field="Capture selection":CHK "$SELECTION" \
+    --field="Capture decorations":CHK "$DECORATIONS" --field="Delay before capture":NUM "$SS_DELAY!0..120")"
+    case $? in
+        0)
+            SELECTION="$(echo $OUTPUT | cut -f4 -d",")"
+            DECORATIONS="$(echo $OUTPUT | cut -f5 -d",")"
+            SS_DELAY="$(echo $OUTPUT | cut -f6 -d",")"
+            savesettings
+            case $(echo $OUTPUT | cut -f2 -d',') in
+                New*)
+                    yadshotcapture
+                    displayss
+                    ;;
+                *File*)
+                    FILE="$(yad --file $PWD --center --title=yadshot --height 600 --width 800)"
+                    case $? in
+                        0)
+                            "$RUNNING_DIR"/teknik.sh "$FILE"
+                            ;;
+                        *)
+                            exit 0
+                            ;;
+                    esac
+                    ;;
+                *Paste*)
+                    PASTE_INPUT="$(yad --form --title="yadshot" --center --height 600 --width 800 --field="":TXT "$(xclip -o -selection clipboard)" --button=gtk-cancel:1 --button="Upload paste"\!gtk-copy:0)"
+                    case $? in
+                        0)
+                            echo "$PASTE_INPUT" | xclip -i -selection clipboard
+                            "$RUNNING_DIR"/teknik.sh -p
+                            ;;
+                        *)
+                            exit 0
+                            ;;
+                    esac
+                    ;;
+                View*)
+                    LIST_ITEM="$(yad --center --list --height 600 --width 800 --title="yadshot" --separator="" --column="Uploads" --button=gtk-close:2 --button="Delete list"\!gtk-delete:1 --button=gtk-copy:0 --rest="$HOME/.teknik")"
+                    case $? in
+                        2)
+                            sleep 0
+                            ;;
+                        1)
+                            yad --center --info --title="yadshot" --button=gtk-ok --text="~/.teknik has been removed!"
+                            rm -f ~/.teknik
+                            ;;
+                        0)
+                            echo -n "$LIST_ITEM" | xclip -i -selection primary
+                            echo -n "$LIST_ITEM" | xclip -i -selection clipboard
+                            yad --center --info --title="yadshot" --button=gtk-ok --text="$LIST_ITEM has been copied to clipboard."
+                            ;;
+                    esac
+                    ;;
+            esac
+            exit 0
+            ;;
+        1)
+            SELECTION="$(echo $OUTPUT | cut -f4 -d",")"
+            DECORATIONS="$(echo $OUTPUT | cut -f5 -d",")"
+            SS_DELAY="$(echo $OUTPUT | cut -f6 -d",")"
+            savesettings
             exit 0
             ;;
     esac
@@ -118,13 +302,13 @@ case $1 in
             while read -r line; do
                 echo -e "$line"
             done | xclip -i -selection clipboard
-            $TEKNIK -p
+            "$RUNNING_DIR"/teknik.sh -p
         else
-            PASTE_INPUT="$(yad --form --title="yadshot" --center --height 600 --width 800 --field="":TXT "$(xclip -o -selection -clipboard)" --button=gtk-cancel:1 --button="Upload paste"\!gtk-copy:0)"
+            PASTE_INPUT="$(yad --form --title="yadshot" --center --height 600 --width 800 --field="":TXT "$(xclip -o -selection clipboard)" --button=gtk-cancel:1 --button="Upload paste"\!gtk-copy:0)"
             case $? in
                 0)
-                    echo "$PASTE_INPUT" | xclip -i -selection -clipboard
-                    $TEKNIK -p
+                    echo "$PASTE_INPUT" | xclip -i -selection clipboard
+                    "$RUNNING_DIR"/teknik.sh -p
                     ;;
                 *)
                     exit 0
@@ -136,7 +320,7 @@ case $1 in
         FILE="$(yad --file $PWD --center --title=yadshot --height 600 --width 800)"
         case $? in
             0)
-                $TEKNIK "$FILE"
+                "$RUNNING_DIR"/teknik.sh "$FILE"
                 ;;
             *)
                 exit 0
@@ -144,10 +328,12 @@ case $1 in
         esac
         ;;
     -c*|--c*)
-        $YADSHOT
+        yadshotcapture
+        displayss
+        exit 0
         ;;
     -t*|--t*)
-        $TRAY
+        yadshottray
         ;;
     *)
         startfunc
